25357538b18157decc6f0f7865e094b3ec4b6bef
S0-HYUN <soh_han@korea.ac.kr>
2021-12-29 05:02:41 +0000

    2021.12.29

---
+++ drawing_pca.py
@@ -0,0 +1,67 @@
+from numpy.core.fromnumeric import repeat
+from sklearn.manifold import TSNE
+import data_loader
+from get_args import Args
+import numpy as np
+import matplotlib.patheffects as PathEffects
+import seaborn as sns
+from sklearn.decomposition import PCA
+import pandas as pd
+
+args_class = Args()
+args = args_class.args
+
+from sklearn.manifold import TSNE
+import time
+import matplotlib.pyplot as plt
+
+def fashion_scatter(x, colors):
+    # choose a color palette with seaborn.
+    num_classes = len(np.unique(colors))
+    palette = np.array(sns.color_palette("hls", num_classes))
+
+    # create a scatter plot.
+    f = plt.figure(figsize=(8, 8))
+    ax = plt.subplot(aspect='equal')
+    sc = ax.scatter(x[:,0], x[:,1], lw=0, s=40, c=palette[colors.astype(np.int)])
+    plt.xlim(-25, 25)
+    plt.ylim(-25, 25)
+    ax.axis('off')
+    ax.axis('tight')
+    plt.savefig("./ddd.png")
+    # add the labels for each digit corresponding to the label
+    txts = []
+
+    # for i in range(num_classes):
+    #     # Position of each label at median of data points.
+    #     xtext, ytext = np.median(x[colors == i, :], axis=0)
+    #     txt = ax.text(xtext, ytext, str(i), fontsize=24)
+    #     txt.set_path_effects([
+    #         PathEffects.Stroke(linewidth=5, foreground="w"),
+    #         PathEffects.Normal()])
+    #     txts.append(txt)
+    
+    return f, ax, sc, txts
+
+time_start = time.time()
+data1 = np.load(f"/opt/workspace/xohyun/MS/Files/Single/Class3/Expt1/day1/subj01.npz")
+x_subset = data1['x']
+print(x_subset.shape)
+x_subset = x_subset.reshape(-1,28)
+y_subset = data1['y']
+y_subset = y_subset.reshape(-1,1)
+print(x_subset.shape)
+print(y_subset.shape)
+
+pca = PCA(n_components=2)
+pca_result = pca.fit_transform(x_subset)
+
+print('PCA done! Time elapsed: {} seconds'.format(time.time()-time_start))
+
+pca_df = pd.DataFrame(columns = ['pca1','pca2'])
+
+pca_df['pca1'] = pca_result[:,0]
+pca_df['pca2'] = pca_result[:,1]
+
+top_two_comp = pca_df[['pca1','pca2']] # taking first and second principal component
+fashion_scatter(top_two_comp.values,y_subset) # Visualizing the PCA output
---
+++ utils.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+import torch
+import random
+import numpy as np
+import torch.backends.cudnn as cudnn
+
+def create_folder(directory) :
+    #---# create folder #---#
+    try : 
+        if not os.path.exists(directory) :
+            os.makedirs(directory)
+    except OSError :
+        print("Error: Creating directory" + directory)
+
+def prepare_folder(path_list) :
+    #---# remove and create folder if aleady exist #---#
+    for pl in path_list :
+        if os.path.isdir(pl) :
+            print(f"[Remove folder] {pl}")
+            shutil.rmtree(pl)
+        create_folder(pl)
+
+def gpu_checking() :
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    os.environ["CUDA_VISIBLE_DEVICES"] = '0, 1, 2, 3'
+    return device
+
+def data_preprocesesing(train, remove, test_list) :
+    train_list = [x for x in train if x not in (test_list)]
+    train_list = [x for x in train_list if x not in (remove)]
+    return train_list
+
+def fix_random_seed(args):
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    # Multi-GPU
+    if args.device == "multi":
+        torch.cuda.manual_seed_all(args.seed)
+    # Single-GPU
+    else:
+        torch.cuda.manual_seed(args.seed)
+    cudnn.benchmark = False  # If you want to set randomness, cudnn.benchmark = False
+    cudnn.deterministic = True  # If you want to set randomness, cudnn.benchmark = True
+    print(f"[Control randomness]\nseed: {args.seed}")
