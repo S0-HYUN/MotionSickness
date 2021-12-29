64770cba66a574be57fe9feccd5af380ceabf734
S0-HYUN <soh_han@korea.ac.kr>
2021-12-29 07:17:47 +0000

    2021.12.29

---
+++ utils_drawing.py
@@ -0,0 +1,20 @@
+import torch
+import numpy as np
+from tsnecuda import TSNE
+import matplotlib.pyplot as plt
+from scipy.stats import wasserstein_distance
+
+def visualizer(data, label, batch=None, dataset=None, tde=-1, title=None):
+    print('T-SNE starts!')
+
+    # data, label = data.cpu().data.numpy(), label
+    x_embedded = TSNE(n_components=2, perplexity=30, learning_rate=10, n_iter=2000).fit_transform(data)
+    plt.figure(figsize=(12, 8))
+
+    label = list(label.squeeze(1))
+    scatter = plt.scatter(x_embedded[:, 0], x_embedded[:, 1], s=3, c=label, cmap='rainbow')
+    plt.legend(handles=scatter.legend_elements()[0], labels=[str(i) for i in range(label[len(label)-1]+1)])
+    plt.show()
+    # if batch is not None:
+    #     plt.savefig('visualization/{}.png'.format(batch), bbox_inches='tight')
+    # plt.savefig('visualization/{}/{}_{}.png'.format(dataset, title, tde), bbox_inches='tight')
